ff6099d8beeb2c778f83a8873f87bdbfaeb3acd0
Shreyash Ravi <sravi@block-tech.io>
2026-06-15 11:44:03 +0200

    enable all features for rust anlayzer

--- lua/plugins/rustacean.lua
+++ lua/plugins/rustacean.lua
@@ -5,11 +5,16 @@ return {
   init = function()
     vim.g.rustaceanvim = {
       server = {
-        cmd = function ()
+        cmd = function()
           local toolchain = vim.fn.trim(vim.fn.system("rustup which rust-analyzer"))
           return { toolchain }
         end,
-      }
+        default_settings = {
+          ['rust-analyzer'] = {
+            cargo = { features = 'all' },
+          },
+        },
+      },
     }
-  end
+  end,
 }
