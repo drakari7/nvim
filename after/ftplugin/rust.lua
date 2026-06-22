2622856d1530e6fd2b196ad5300aa01bd0971eb2
Shreyash Ravi <sravi@block-tech.io>
2026-06-22 13:14:27 +0200

    add some rust keybinds

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -3,5 +3,7 @@ buf_map('n', '<leader>rr', ':w<CR>:!cargo run<CR>', 'Execute file')
 
 buf_map("n", "gra", function() vim.cmd.RustLsp("codeAction") end, "Code Action")
 buf_map("n", "K", function() vim.cmd.RustLsp({ "hover", "actions" }) end, "Hover")
+buf_map("n", "<leader>rd", function() vim.cmd.RustLsp("relatedDiagnostics") end, "Related Diagnostics")
+buf_map("n", "<leader>rc", function() vim.cmd.RustLsp("openCargo") end, "Open Cargo.toml")
 
--- Format-on-save handled by conform.nvim (lsp_format = 'fallback' routes to rust-analyzer).
+-- Format-on-save handled by conform.nvim
